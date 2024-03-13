929600accf577fc2f1627271517de83382f3f718
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2024-03-13 17:17:10 +0530

    Update DepthFirstSearch.py

--- Python/AI_expt/DepthFirstSearch.py
+++ Python/AI_expt/DepthFirstSearch.py
@@ -8,17 +8,17 @@ tree = {
     'G': {'I'},
 }
 
-goal_nodes = ['D', 'G']
+goal_nodes = {'D', 'G'}  # Use a set for faster membership checking
 first_node = 'A'
 active_node = first_node
 
 open_stack = [first_node]
-close_list = set()  # Use a set for faster membership checking
+close_list = set()
 
 def movegen(current_node):
     global active_node
 
-    close_list.add(current_node)  # Add the current node to the set of explored nodes
+    close_list.add(current_node)
 
     # Explore children (in reverse order for DFS) that haven't been explored
     if current_node in tree:
@@ -29,7 +29,7 @@ def movegen(current_node):
             if child not in close_list:
                 open_stack.insert(0, child)
 
-        active_node = open_stack[0]  # Set active node as the first node in the open stack
+        active_node = open_stack[0]
 
 def Goal_test(current_node, target_nodes):
     if current_node in target_nodes:
