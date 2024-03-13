765caf199c49c37061c205d78c88d379d7335aac
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2024-03-13 17:13:53 +0530

    Update BreadthFirstSearch.py

--- Python/AI_expt/BreadthFirstSearch.py
+++ Python/AI_expt/BreadthFirstSearch.py
@@ -1,14 +1,14 @@
 tree = {
-    'A': {'B', 'C'},
-    'B': {'D', 'E'},
-    'C': {'F', 'G'},
-    'D': {'H'},
-    'E': {None},
-    'F': {None},
-    'G': {'I'},
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F', 'G'],
+    'D': ['H'],
+    'E': [None],
+    'F': [None],
+    'G': ['I'],
 }
 
-goal_nodes = ['D', 'G']  
+goal_nodes = ['D', 'G']
 first_node = 'A'
 active_node = first_node
 
@@ -18,16 +18,16 @@ close_list = []
 def movegen(current_node):
     global active_node
     close_list.insert(0, current_node)
-    open_list.pop(0)  #pop first node in the open list
+    open_list.pop(0)  # pop first node in the open list
 
-    #Explore children
+    # Explore children
     if current_node in tree:
         open_list.extend(tree[current_node])
-        active_node = open_list[len(open_list) - 1]  #set active node as the last node in the open list
+        active_node = open_list[-1]  # set active node as the last node in the open list
 
 def Goal_test(current_node, target_nodes):
     if current_node in target_nodes:
-        close_list.insert(0, current_node)  #add the current node as the last node in the sequence
+        close_list.insert(0, current_node)  # add the current node as the last node in the sequence
         print("GOAL FOUND:", current_node)
         target_nodes.remove(current_node)  # remove the found goal node from the list
     else:
