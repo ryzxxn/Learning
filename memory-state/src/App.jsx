6426386a2eb9596dc69704e1b7f7447b5b2c21c0
Elton Costa <89019551+ryzxxn@users.noreply.github.com>
2024-06-09 22:07:13 +0530

    adsad

--- memory-state/src/App.jsx
+++ memory-state/src/App.jsx
@@ -5,26 +5,8 @@ import Test from './pages/test';
 import memoryState from 'memory-state';
 
 function App() {
-  const [currentLocation, setCurrentLocation] = useState(window.location.pathname);
 
-  useEffect(() => {
-    const handleLocationChange = () => {
-      setCurrentLocation(window.location.pathname);
-
-      if (sessionStorage.getItem('random')) {
-        memoryState.setState('user', {name: 'Jhon', age: '30', random: sessionStorage.getItem('random')})
-      }
-      sessionStorage.clear()
-    };
-
-    window.addEventListener('popstate', handleLocationChange);
-
-    return () => {
-      window.removeEventListener('popstate', handleLocationChange);
-    };
-  }, []);
-
-  memoryState.setState('user', { name: 'John', age: 30 });
+  memoryState.setState('user', { name: 'John', age: 69 });
 
   return (
     <BrowserRouter>
